71d9b8d7362d5a75f95daa2c8ca53cfc1883f9f2
10152132 <hyejoo.jung@kt.com>
2022-12-14 16:21:03 +0900

    Add TestClient while true

--- src/TestClient.java
+++ src/TestClient.java
@@ -6,26 +6,31 @@ import java.net.UnknownHostException;
 public class TestClient {
     public static void main(String[] args) {
         System.out.println("Client ON");
+        while (true) {
+            try {
+                String message;
 
-        try {
-            String message;
+                Socket socket = new Socket("127.0.0.1", 5001);
+                OutputStream out = socket.getOutputStream();
+                message = "0x5001|anny|22";
+                out.write(message.getBytes());
+                socket.close();
 
-            Socket socket = new Socket("127.0.0.1", 5001);
-            OutputStream out = socket.getOutputStream();
-            message = "0x5001|anny|22";
-            out.write(message.getBytes());
-            socket.close();
+                Socket socket2 = new Socket("127.0.0.1", 5001);
+                OutputStream out2 = socket2.getOutputStream();
+                message = "0x6001|jung|1234|anny|27|female";
+                out2.write(message.getBytes());
+                socket2.close();
 
-            Socket socket2 = new Socket("127.0.0.1", 5001);
-            OutputStream out2 = socket2.getOutputStream();
-            message = "0x6001|jung|1234|anny|27|female";
-            out2.write(message.getBytes());
-            socket2.close();
+                Thread.sleep(100);
 
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
